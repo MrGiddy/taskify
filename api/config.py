0f6fc0dea1753d857be01a5e91110cbe2fcbaa7a
Matumbai Binale <binalematumbai@gmail.com>
2024-06-27 01:19:44 +0300

    (fix): Ensure database exists

--- api/config.py
+++ api/config.py
@@ -2,6 +2,12 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+db_path = os.path.join(basedir, '..', 'taskify.db')
+
+# Ensure the directory for the database exists
+if not os.path.exists(os.path.dirname(db_path)):
+    os.makedirs(os.path.dirname(db_path))
+
 class Config:
     JSONIFY_PRETTYPRINT_REGULAR = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'taskify.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + db_path
